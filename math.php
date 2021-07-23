a99a89be86b02ebc06eceebeaeae39ba3300a406
Mystic Kitty <djabranton@gmail.com>
2021-07-23 16:37:04 -0700

    Math Operations.

---
+++ math.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+echo 56 + 45;
+echo "<br>";
+echo 56 - 45;
+echo "<br>";
+echo 56 * 45;
+echo "<br>";
+echo 56 / 45;
+echo "<br>";
+echo "<br>";
+echo 45 + 34 * 45/421-45;
+echo "<br>";
+echo (5 + 5) * 10; // order of operations
+
+$number1 = 12;
+$number2 = 24;
+echo "<br>";
+echo $number1 + $number2;
+
+
+?>
+
+    
+</body>
+</html>
+
+

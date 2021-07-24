18af4130ef33e452c5a853c1d70f409b5bea3374
Mystic Kitty <djabranton@gmail.com>
2021-07-23 17:02:24 -0700

    Assoc Arrays.

---
+++ arrays_assoc.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php  
+
+$number = array(10, 20, 49);
+
+
+echo $number[2] . '<br>';
+
+//key, value
+$names = array('first_name' => 'Edwin', 'last_name' => 'Diaz') ;
+
+print_r($names);
+
+echo $names['first_name'] . " " . $names['last_name'];
+
+
+?>
+
+    
+</body>
+</html>
+
+

f7d25243f0285593336fe12d826129b2c3a2a3c0
Mystic Kitty <djabranton@gmail.com>
2021-08-24 15:58:33 -0700

    if statement

---
+++ ifstatement.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+if(3 > 10){
+
+    echo "three is less than 10";
+
+
+}elseif(4 < 5) {
+    echo "of course 4 is less than 5";
+}
+
+?>
+
+    
+</body>
+</html>
+
+
